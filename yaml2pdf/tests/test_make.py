424971301452a303cbdd228e3a427341470184a2
Xavier Duran <xavier.duran@pm.me>
2022-08-20 22:34:09 +0200

    fix: add missing output directory parameter to tests

--- yaml2pdf/tests/test_make.py
+++ yaml2pdf/tests/test_make.py
@@ -3,6 +3,8 @@ import unittest
 
 from yaml2pdf.make import make_document_from_directory, make_document_from_yaml
 
+import tempfile
+
 
 class TestYAML2PDF(unittest.TestCase):
     def test_single(self):
@@ -13,14 +15,17 @@ class TestYAML2PDF(unittest.TestCase):
             os.path.join(
                 os.path.dirname(__file__), "../samples/data/invoices.yaml"
             ),
+            output_dir=tempfile.mkdtemp(),
         )
 
     def test_directory(self):
+
         make_document_from_directory(
             os.path.join(
                 os.path.dirname(__file__), "../samples/template/index.html"
             ),
             os.path.join(os.path.dirname(__file__), "../samples/data"),
+            output_dir=tempfile.mkdtemp(),
         )
 
 
